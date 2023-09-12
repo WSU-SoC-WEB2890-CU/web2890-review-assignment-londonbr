f00a8ed56e59a4497ac3f1c8742ddb1bc6295dbf
Brinlee London <brinleelondon@mail.weber.edu>
2023-09-11 23:13:10 -0600

    final submission

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,7 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import 'bootstrap'
+
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl, option)
+})
